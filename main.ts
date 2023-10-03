4846384a9a638b29b9b223a7c4939fb942a5f98f
jenifferpaola <146844807+jenifferpaola@users.noreply.github.com>
2023-10-03 07:58:32 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+let Dado = 0
+input.onGesture(Gesture.Shake, function () {
+    Dado = randint(0, 2)
+    if (Dado == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (Dado == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Dado == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
